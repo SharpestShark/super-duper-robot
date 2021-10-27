a2e951b9e40f1b46817c631527a97a92ef858ce4
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-10-27 08:04:51 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function start() {
+  scene.start();
+}
+var scene = {
+  canvas: document.createElement("canvas"),
+  start: function() {
+    this.canvas.width = 800;
+    this.canvas.height = 1000;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
+  
