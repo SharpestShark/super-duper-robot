97d0fd6ef2facf905e4b84e305d1a1df762f0c08
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-10-27 08:17:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+var player;
+
 function start() {
   scene.start();
+  player = new component(30, 30, "red", 10, 120);
 }
+
 var scene = {
   canvas: document.createElement("canvas"),
   start: function() {
@@ -8,6 +12,26 @@ var scene = {
     this.canvas.height = 1000;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateScene, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
-  
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y; 
+  this.update = function(){
+    ctx = scene.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function updateScene() {
+  scene.clear();
+  player.update();
+}
