94ceb13eabe1c167cae7e82a0feb09a6baf3b33b
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-02 12:53:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -205,8 +205,6 @@ var x, y, width, height, gap, minWidth, maxWidth, minHeight, maxHeight, minGap, 
     myObstacles[i].update();
   }
   if (navigator.platform.startsWith('Win')) {
-    player.speedX = 0;
-    player.speedY = 0;
     if (scene.keys && scene.keys[37] || scene.keys[65]) {player.x -= 1; }
     if (scene.keys && scene.keys[39] || scene.keys[68]) {player.x += 1; }
     if (scene.keys && scene.keys[38] || scene.keys[87]) {player.y -= 1; }
