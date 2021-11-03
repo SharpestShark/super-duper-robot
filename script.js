06aed5703ed5b0d17e526aec7b41c66d58c0ef92
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-03 16:33:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ var background = {
 function start() {
   player = new component(30, 30, "red", 10, 120);
   health = new component(100,20,"green", 10, 400);
-  hpVis = new component("12px", "Consolas", "white", 12, 390, "text");
   health.value = 100;
+  hpVis = new component("12px", "Consolas", "black", 12, 390, "text");
   tree = background.tree;
   rock = background.rock;
 	var x = 20;
