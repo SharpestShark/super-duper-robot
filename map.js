5d4960f044639d88e03fc3d465f10909e1418dba
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-04 13:21:46 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,16 +1,14 @@
+function map() {
 var map = [];
 var i;
 var x = scene.canvas.width;
 var y = scene.canvas.height;
-document.body.onload(function() {
 var ctx = document.getElementsByTagName('canvas')[0].getContext("2d");
-//var ctx = c.getContext('2d');
-});
 ctx.beginPath();
 for (i = 0;i < 10;i++) {
   x -= Math.floor(Math.random()*(300+1));
   y -= Math.floor(Math.random()*(100+1));
-  map.push('{' + i + ': {"x": ' + x + ',"y": ' + y + '}}');
+  map.push(i + ': {"x": ' + x + ',"y": ' + y + '}');
   if (i != 0) {
     ctx.lineTo(map[i].x,map[i].y);
   } else {
@@ -19,3 +17,4 @@ for (i = 0;i < 10;i++) {
 }
 ctx.stroke();
 console.log(map);
+}
