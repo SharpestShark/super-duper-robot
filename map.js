ddda00709b6e8485f3a558f27627e78f3b8cbd86
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-04 13:12:20 -0700

    Update map.js

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ ctx.beginPath();
 for (i = 0;i < 10;i++) {
   x -= Math.floor(Math.random()*(300+1));
   y -= Math.floor(Math.random()*(100+1));
-  map.push(i + ': {"x": ' + x + ',"y": ' + y + '}');
+  map.push('{' + i + ': {"x": ' + x + ',"y": ' + y + '}}');
   if (i != 0) {
     ctx.lineTo(map[i].x,map[i].y);
   } else {
