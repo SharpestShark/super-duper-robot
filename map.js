4dc2a7ef440adfec70146da76d5bfc8ac2046393
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-04 10:42:04 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
 var map = [];
 var i;
 for (i = 0;i < 10;i++) {
-  var x = Math.floor(Math.random()*(scene.canvas.height-200+1)+200);
-  var y = Math.floor(Math.random()*(scene.canvas.height-200+1)+200);
+  var x = scene.canvas.width;
+  var y = scene.canvas.height;
+  x -= Math.floor(Math.random()*(x+1));
+  y -= Math.floor(Math.random()*(y+1));
   map.push(i + ': {"x": ' + x + ',"y": ' + y + '}');
 }
 console.log(map);
