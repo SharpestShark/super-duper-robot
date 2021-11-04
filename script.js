e19746ebda548b6df26386c554ea2dcc735e2672
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-04 07:28:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ var x, y, width, height, gap, minWidth, maxWidth, minHeight, maxHeight, minGap, 
         health.value += 5;
 	health.width = health.value;
 	      if (health >= 50) {
-		      
+	        health.color = "green";
 	      }
       }
       myObstacles[i].exist = false;
