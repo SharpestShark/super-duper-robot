ee8b84177a50b7511dd9206aec0f008fa7358830
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-11-10 18:25:49 -0700

    Update map.js

--- map.js
+++ map.js
@@ -4,17 +4,24 @@ var i;
 var x = scene.canvas.width;
 var y = scene.canvas.height;
 var ctx = document.getElementsByTagName('canvas')[0].getContext("2d");
-for (i = 0;i < 10;i++) {
-  x -= Math.floor(Math.random()*(300+1));
-  y -= Math.floor(Math.random()*(100+1));
-  map.push({"x": x,"y": y});
-  if (i != 0) {
-    ctx.lineTo(map[i].x,map[i].y);
-  } else {
-    ctx.moveTo(300,300);
+if (map.length == 0) {
+  for (i = 0;map.length < 10; i++) {
+    x -= Math.floor(Math.random()*(300+1));
+    y -= Math.floor(Math.random()*(100+1));
+    map.push({"x": x,"y": y});
   }
-  ctx.stroke();
+  window.localStorage.setItem('map',map);
+} else {
+  for (i = 0;i < 10;i++) {
+    ctx.beginPath();
+    if (i != 0) {
+      ctx.lineTo(map[i].x,map[i].y);
+    } else {
+      ctx.moveTo(300,300);
+    }
+    ctx.stroke();
+  }
+  console.log(map);
+  console.log(map[1].x);
 }
-console.log(map);
-console.log(map[1].x);
 }
