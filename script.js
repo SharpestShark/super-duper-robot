724b53220624cbcce3f143494a282d71d0e418b9
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-12-09 20:58:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -372,16 +372,17 @@ var x, y, scale, isObstacle;
   for (var i = 0; i < mySpeed.length; i += 1) {
     if (player.crashWith(mySpeed[i]) && (mySpeed[i].exist == true)) {
       player.stat = new component("20px","Consolas","black",player.x,player.y - 20,"text");
-      player.stat.text = speedT.toString();
-      player.stat.exist = false;
       if (speedT < 2) {
         if (mySpeed[i].sped == 1) {
           speedT += 0.2;
         }
 	      if ((speedT > 1) && (speedT <= 2)) {
           player.color = "blue";
+          player.stat.text = speedT.toString();
           player.stat.exist = true;
-	      }
+	      } else if ((speedT <= 1) && (speedT > 0)) {
+          player.stat.exist = false;
+        }
       }
       mySpeed[i].exist = false;
       d = new Date();
@@ -574,7 +575,7 @@ var x, y, scale, isObstacle;
   interactBtn.x = player.x + player.width;
   interactBtn.y = player.y + player.height;
   interactBtn.update();
-  if (speedT > 1) {
+  if ((player.stat) && (speedT > 1)) {
     player.stat.update();
   }
   /*scene.context.beginPath();
